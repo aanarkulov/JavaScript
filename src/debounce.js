eb1fbbfcd6151fadf104b899ad4b549d50c3225c
aanarkulov <a.anarkuloff@gmail.com>
2023-04-05 11:27:56 +0600

    first commit

---
+++ src/debounce.js
@@ -0,0 +1,27 @@
+// debounce - прерывать предыдущие действия и отложенно выполнять последнее
+
+
+const fetchUrl = (url) => {
+  console.log(`fetching ${url}`);
+};
+
+function debounce(callback, delay) {
+  let timer = null;
+
+  return (...args) => {
+    if (timer) {
+      clearTimeout(timer)
+    }
+    timer = setTimeout(() => {
+      callback(...args)
+    }, delay);
+  }
+}
+
+const fetching = debounce(fetchUrl, 300);
+
+fetching(1);
+fetching(2);
+fetching(3);
+fetching(4);
+fetching(5);
